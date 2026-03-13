34c5e9e92b045fe24d757c3758804c177a580f53
Pramila B <ppbha811@gmail.com>
2026-03-13 22:17:04 +0300

    Initial commit

---
+++ src/main/java/com/pipeline/datapipelinesystem/DataPipelineSystemApplication.java
@@ -0,0 +1,13 @@
+package com.pipeline.datapipelinesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataPipelineSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataPipelineSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pipeline/datapipelinesystem/DataPipelineSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipeline.datapipelinesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataPipelineSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
